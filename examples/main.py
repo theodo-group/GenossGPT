dafac4474c982df38d4bd2314fdd82a48f584aae
Stan Girard <girard.stanislas@gmail.com>
2023-07-19 13:40:31 +0200

    feat(example): changed name to Genoss

--- examples/main.py
+++ examples/main.py
@@ -13,7 +13,7 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot")
+st.title("🐂🌈 Genoss")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "assistant", "content": "How can I help you?"}
