5a7505aaa1e8434d65570be90c28ee0869ef5f58
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-07-20 15:57:39 +0100

    fix: test

--- genoss/services/model_factory.py
+++ genoss/services/model_factory.py
@@ -1,8 +1,11 @@
 from typing import Optional
 
-from genoss.llm.base_genoss_llm import BaseGenossLLM
+from genoss.llm.base_genoss import BaseGenossLLM
 from genoss.llm.fake_llm import FAKE_LLM_NAME, FakeLLM
-from genoss.llm.local.gpt4all_llm import Gpt4AllLLM
+from genoss.llm.hf_hub.falcon import HuggingFaceHubFalconLLM
+from genoss.llm.hf_hub.gpt2 import HuggingFaceHubGPT2LLM
+from genoss.llm.hf_hub.llama2 import HuggingFaceHubLlama2LLM
+from genoss.llm.local.gpt4all import Gpt4AllLLM
 from genoss.llm.openai.openai_llm import OpenAILLM
 
 OPENAI_NAME_LIST = ["gpt-4", "gpt-3.5-turbo"]
@@ -10,7 +13,7 @@ OPENAI_NAME_LIST = ["gpt-4", "gpt-3.5-turbo"]
 
 class ModelFactory:
     @staticmethod
-    def get_model_from_name(name: str) -> Optional[BaseGenossLLM]:
+    def get_model_from_name(name: str, api_key) -> Optional[BaseGenossLLM]:
         if name.lower() in OPENAI_NAME_LIST:
             return OpenAILLM(model_name=name, api_key=api_key)
         if name.lower() == "gpt4all":
