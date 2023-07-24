b636bfcff6414d9ada972b48b322c89efdbf4c79
Arnault Chazareix <arnaultc@sicara.com>
2023-07-24 18:49:10 +0200

    quality(): change tests to pytest and use mark parametrize

--- tests/services/test_model_factory.py
+++ tests/services/test_model_factory.py
@@ -1,5 +1,6 @@
-import unittest
+import pytest
 
+from genoss.llm.base_genoss import BaseGenossLLM
 from genoss.llm.fake_llm import FAKE_LLM_NAME, FakeLLM
 from genoss.llm.hf_hub.falcon import HuggingFaceHubFalconLLM
 from genoss.llm.hf_hub.gpt2 import HuggingFaceHubGPT2LLM
@@ -9,31 +10,24 @@ from genoss.llm.openai.openai_llm import OpenAILLM
 from genoss.services.model_factory import ModelFactory
 
 
-class TestModelFactory(unittest.TestCase):
-    def test_get_model_from_name_hf_gpt2(self):
-        model = ModelFactory.get_model_from_name("hf-gpt2", "api_key")
-        self.assertIsInstance(model, HuggingFaceHubGPT2LLM)
+class TestModelFactory:
+    @staticmethod
+    @pytest.mark.parametrize(
+        ("model_name", "model_class"),
+        [
+            ("hf-gpt2", HuggingFaceHubGPT2LLM),
+            ("hf-falcon", HuggingFaceHubFalconLLM),
+            ("hf-llama2", HuggingFaceHubLlama2LLM),
+            ("gpt-4", OpenAILLM),
+            ("Gpt4all", Gpt4AllLLM),
+            (FAKE_LLM_NAME, FakeLLM),
+        ],
+    )
+    def test_get_model_from_name(model_name: str, model_class: type[BaseGenossLLM]):
+        model = ModelFactory.get_model_from_name(name=model_name, api_key="api_key")
+        assert isinstance(model, model_class)
 
-    def test_get_model_from_name_hf_falcon(self):
-        model = ModelFactory.get_model_from_name("hf-falcon", "api_key")
-        self.assertIsInstance(model, HuggingFaceHubFalconLLM)
-
-    def test_get_model_from_name_hf_llama2(self):
-        model = ModelFactory.get_model_from_name("hf-llama2", "api_key")
-        self.assertIsInstance(model, HuggingFaceHubLlama2LLM)
-
-    def test_get_model_from_name_openai(self):
-        model = ModelFactory.get_model_from_name("gpt-4", "api_key")
-        self.assertIsInstance(model, OpenAILLM)
-
-    def test_get_model_from_name_gpt4all(self):
-        model = ModelFactory.get_model_from_name("Gpt4all")
-        self.assertIsInstance(model, Gpt4AllLLM)
-
-    def test_get_model_from_name_fake(self):
-        model = ModelFactory.get_model_from_name(FAKE_LLM_NAME)
-        self.assertIsInstance(model, FakeLLM)
-
-    def test_get_model_from_name_unknown(self):
+    @staticmethod
+    def test_get_model_from_name_unknown():
         model = ModelFactory.get_model_from_name("unknown", "")
-        self.assertIsNone(model)
+        assert model is None
