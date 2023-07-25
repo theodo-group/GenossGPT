48a4cd3b7eda7d0188d16b02f2212d6ebe8e3ea2
Arnault Chazareix <arnaultc@sicara.com>
2023-07-25 23:59:27 +0200

    quality(): simplify model inheritence

--- genoss/llm/hf_hub/base_hf_hub.py
+++ genoss/llm/hf_hub/base_hf_hub.py
@@ -1,9 +1,7 @@
 from abc import ABC
 from typing import Any
 
-from fastapi import HTTPException
 from langchain import HuggingFaceHub, LLMChain
-from pydantic import Field
 
 from genoss.entities.chat.chat_completion import ChatCompletion
 from genoss.llm.base_genoss import BaseGenossLLM
@@ -14,22 +12,13 @@ class BaseHuggingFaceHubLLM(BaseGenossLLM, ABC):
     """Class for interacting with Hugging Face Inference APIs."""
 
     # Sub classes must define these
-    huggingfacehub_api_token: str | None = Field(None)
-    repo_id: str | None = None
-
-    def __init__(self, api_key: str | None, *args: Any, **kwargs: Any):
-        super().__init__(*args, **kwargs)
-
-        if api_key is None:
-            # TODO: is this the right way to make it fail?
-            raise HTTPException(status_code=403, detail="API key missing")
-
-        self.huggingfacehub_api_token = api_key
+    api_key: str | None = None
+    repo_id: str
 
     def generate_answer(self, question: str) -> dict[str, Any]:
         """Generate answer from prompt."""
         llm = HuggingFaceHub(
-            repo_id=self.repo_id, huggingfacehub_api_token=self.huggingfacehub_api_token
+            repo_id=self.repo_id, huggingfacehub_api_token=self.api_key
         )
         llm_chain = LLMChain(prompt=prompt_template, llm=llm)
 
