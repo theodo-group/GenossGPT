17a4cc5ce4e6bc1eb510f6a3664fac5c2102d1de
Stan Girard <girard.stanislas@gmail.com>
2023-07-28 14:38:22 +0200

    fix(demo): changed env variable so it works everywhere

--- demo/constants/model_configs.py
+++ demo/constants/model_configs.py
@@ -20,31 +20,31 @@ AVAILABLE_MODELS = [
     ModelConfig(
         display_name="OpenAI-GPT-4",
         model_name="gpt-4",
-        api_key=SETTINGS.openai_api_key,
+        api_key=SETTINGS.genoss_openai_api_key,
         endpoint_url=openai.api_base,
     ),
     ModelConfig(
         display_name="OpenAI-GPT-4 (through Genoss)",
         model_name="gpt-4",
-        api_key=SETTINGS.openai_api_key,
+        api_key=SETTINGS.genoss_openai_api_key,
         endpoint_url=GENOSS_URL,
     ),
     ModelConfig(
         display_name="hf-gpt2",
         model_name="hf-gpt2",
-        api_key=SETTINGS.huggingfacehub_api_token,
+        api_key=SETTINGS.genoss_huggingfacehub_api_token,
         endpoint_url=GENOSS_URL,
     ),
     ModelConfig(
         display_name="hf-llama2",
         model_name="hf-llama2",
-        api_key=SETTINGS.huggingfacehub_api_token,
+        api_key=SETTINGS.genoss_huggingfacehub_api_token,
         endpoint_url=GENOSS_URL,
     ),
     ModelConfig(
         display_name="hf-custom/llama",
-        model_name=f"hf-inference-endpoint/{SETTINGS.custom_hf_endpoint_url}",
-        api_key=SETTINGS.huggingfacehub_api_token,
+        model_name=f"hf-inference-endpoint/{SETTINGS.genoss_custom_hf_endpoint_url}",
+        api_key=SETTINGS.genoss_huggingfacehub_api_token,
         endpoint_url=GENOSS_URL,
     ),
 ]
