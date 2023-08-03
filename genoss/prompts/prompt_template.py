ef3bd40b5213a5b74d5bf38ef2434f43e5eec4ef
Stan Girard <girard.stanislas@gmail.com>
2023-08-03 11:01:18 +0200

    feat(prompt): answer in language of the question

--- genoss/prompts/prompt_template.py
+++ genoss/prompts/prompt_template.py
@@ -1,7 +1,7 @@
 from langchain import PromptTemplate
 
 system_prompt = """\
-    You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. \
+    You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Answer in the language of the question \
     Here is the question from the user: {question}\
     Answer:\
 """
