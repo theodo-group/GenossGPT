28e3663d49e28ce745f8b709d42654258e5d2c88
Arnault Chazareix <arnaultc@sicara.com>
2023-08-23 16:20:13 +0200

    feat(): add genoss server scripts entry point

---
+++ genoss/commands/start_genoss_server.py
@@ -0,0 +1,147 @@
+import click
+import uvicorn
+from uvicorn.config import LOGGING_CONFIG
+from uvicorn.main import LEVEL_CHOICES
+
+from genoss.default_server import app
+
+
+@click.command(context_settings={"auto_envvar_prefix": "GENOSS"})
+@click.option(
+    "--host",
+    type=str,
+    default="127.0.0.1",
+    help="Bind socket to this host.",
+    show_default=True,
+)
+@click.option(
+    "--port",
+    type=int,
+    default=8000,
+    help="Bind socket to this port. If 0, an available port will be picked.",
+    show_default=True,
+)
+@click.option("--reload", is_flag=True, default=False, help="Enable auto-reload.")
+@click.option(
+    "--reload-dir",
+    "reload_dirs",
+    multiple=True,
+    help="Set reload directories explicitly, instead of using the current working"
+    " directory.",
+    type=click.Path(exists=True),
+)
+@click.option(
+    "--reload-include",
+    "reload_includes",
+    multiple=True,
+    help="Set glob patterns to include while watching for files. Includes '*.py' "
+    "by default; these defaults can be overridden with `--reload-exclude`. "
+    "This option has no effect unless watchfiles is installed.",
+)
+@click.option(
+    "--reload-exclude",
+    "reload_excludes",
+    multiple=True,
+    help="Set glob patterns to exclude while watching for files. Includes "
+    "'.*, .py[cod], .sw.*, ~*' by default; these defaults can be overridden "
+    "with `--reload-include`. This option has no effect unless watchfiles is "
+    "installed.",
+)
+@click.option(
+    "--reload-delay",
+    type=float,
+    default=0.25,
+    show_default=True,
+    help="Delay between previous and next check if application needs to be."
+    " Defaults to 0.25s.",
+)
+@click.option(
+    "--workers",
+    default=None,
+    type=int,
+    help="Number of worker processes. Defaults to the $WEB_CONCURRENCY environment"
+    " variable if available, or 1. Not valid with --reload.",
+)
+@click.option(
+    "--env-file",
+    type=click.Path(exists=True),
+    default=None,
+    help="Environment configuration file.",
+    show_default=True,
+)
+@click.option(
+    "--log-config",
+    type=click.Path(exists=True),
+    default=None,
+    help="Logging configuration file. Supported formats: .ini, .json, .yaml.",
+    show_default=True,
+)
+@click.option(
+    "--log-level",
+    type=LEVEL_CHOICES,
+    default=None,
+    help="Log level. [default: info]",
+    show_default=True,
+)
+@click.option(
+    "--access-log/--no-access-log",
+    is_flag=True,
+    default=True,
+    help="Enable/Disable access log.",
+)
+@click.option(
+    "--use-colors/--no-use-colors",
+    is_flag=True,
+    default=None,
+    help="Enable/Disable colorized logging.",
+)
+def main(
+    host: str,
+    port: int,
+    reload: bool,
+    reload_dirs: list[str],
+    reload_includes: list[str],
+    reload_excludes: list[str],
+    reload_delay: float,
+    workers: int,
+    env_file: str,
+    log_config: str,
+    log_level: str,
+    access_log: bool,
+    use_colors: bool,
+) -> None:
+    """A simple entrypoint for the default genoss server.
+
+    This is configured in `pyproject.toml:tool.poetry.scripts:genoss-server` .
+    You can use it this way
+    ```sh
+    genoss-server
+    genoss-server --help
+    genoss-server --log-level debug --reload
+    ```
+
+    This is heavily inspired by uvicorn.main / run which have a lot more options.
+    To do more, the best solution is to use genoss as it is : a fastapi app.
+    You can then configure it as you want and rely on powerful tools provided by
+    fastapi, uvicorn or other ASGI tool.
+    """
+    uvicorn.run(
+        app=app,
+        host=host,
+        port=port,
+        reload=reload,
+        reload_dirs=reload_dirs or None,
+        reload_includes=reload_includes or None,
+        reload_excludes=reload_excludes or None,
+        reload_delay=reload_delay,
+        workers=workers,
+        env_file=env_file,
+        log_config=LOGGING_CONFIG if log_config is None else log_config,
+        log_level=log_level,
+        access_log=access_log,
+        use_colors=use_colors,
+    )
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from genoss.default_server import app
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app)
+    uvicorn.run(app=app)
